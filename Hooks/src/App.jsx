335327e982355a77a67b9e96efc019a1a77c9081
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-07-11 01:27:53 +0530

    Color change hook

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import {React, useState} from 'react'
 
 function App() {
+
+
+  const [color,setColor] = useState("blue");
+  function handleClick(){
+        setColor("black");
+  }
   return (
     <div>
-      
+      <h1>Color changed to: {color}</h1>
+      <button onClick={handleClick}>Black</button>
     </div>
   )
 }
