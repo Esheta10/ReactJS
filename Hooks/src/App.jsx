20d911add609bd86654ee890b8a1ac6b7ad4077b
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-07-11 14:14:20 +0530

    StopWatch

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -1,19 +1,35 @@
-import {React, useState, useEffect} from 'react'
+import {React, useState, useRef} from 'react'
 
 function App() {
-  
-  const [count1, setCount1] = useState(0);
-  const [count2, setCount2] = useState(0);
-  
-useEffect(()=>{
-console.log("Renders on mount and then only when the value of dependency, here --> count1 changes but for count2 it won't run on every change since its not present in dependency");
-},[count1])
+
+  const [time, setTime] = useState(0);
+  const timeRef = useRef(null);
+
+  function startTimer(){
+    timeRef.current = setInterval(()=>{
+      setTime((prevTime) => prevTime + 1)
+    }, 1000)
+  }
+
+  function stopTimer(){
+    clearInterval(timeRef.current);
+    timeRef.current = null;
+  }
+
+  function resetTimer(){
+      stopTimer();
+      setTime(0);
+  }
+
   return (
     <div>
-      <button onClick={()=>setCount1(count1+1)}> Count1: {count1}</button> <br/> <br/>
-      <button onClick={()=>setCount2(count2+1)}> Count2: {count2}</button>
+      <h1>Stop-watch : {time} seconds</h1>
+      <button onClick={startTimer}>Start Timer</button> <br/> <br/>
+      <button onClick={stopTimer}>Stop Timer</button> <br/> <br/>
+      <button onClick={resetTimer}>Reest Timer</button> <br/> <br/>
     </div>
   )
 }
 
 export default App
+
