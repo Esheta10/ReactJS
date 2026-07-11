f8cd0ae409e1fd0ae08d18f9d9de89489cfd02f6
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-07-11 12:23:22 +0530

    useEffect Hook

--- Hooks/src/App.jsx
+++ Hooks/src/App.jsx
@@ -1,16 +1,17 @@
-import {React, useState} from 'react'
+import {React, useState, useEffect} from 'react'
 
 function App() {
-
-
-  const [color,setColor] = useState("blue");
-  function handleClick(){
-        setColor("black");
-  }
+  
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+  
+useEffect(()=>{
+console.log("Renders on mount and then only when the value of dependency, here --> count1 changes but for count2 it won't run on every change since its not present in dependency");
+},[count1])
   return (
     <div>
-      <h1>Color changed to: {color}</h1>
-      <button onClick={handleClick}>Black</button>
+      <button onClick={()=>setCount1(count1+1)}> Count1: {count1}</button> <br/> <br/>
+      <button onClick={()=>setCount2(count2+1)}> Count2: {count2}</button>
     </div>
   )
 }
