43a006a099adebf772c8978e9249c525b4cf0119
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-07-18 18:23:32 +0530

    commit

---
+++ Profile-Card-UI-Design/src/components/UserCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserCard() {
+  return (
+    <div>
+      <div className="h-[800px] w-[400px] bg-blue-400"></div>
+    </div>
+  )
+}
+
+export default UserCard
