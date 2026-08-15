bf33ed72d1d25e340dabfc7244b1b27824005558
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-08-15 12:17:31 +0530

    More on Props

---
+++ More_on_Props/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './components/Card'
+import "./App.css"
+const App = () => {
+  return (
+    <div className="layout">
+      <Card name="Esheta Jaiswal" />
+      <Card name="Riya Soni" />
+      <Card name="Ritocheta Biswas" />
+      <Card name="Sangjukta Sarkar" />
+    </div>
+  )
+}
+
+export default App
---
+++ More_on_Props/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = ({name}) => {
+
+  return (
+    <div>
+      <div className="card">
+        {name}
+      </div>
+    </div>
+  )
+}
+
+export default Card
