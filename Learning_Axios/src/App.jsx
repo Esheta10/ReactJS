21d3cbb5967b27166e8a938cebfc5ccbc6b36cad
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-08-26 12:32:43 +0530

    Fetching Data using fetch-JS library

---
+++ Learning_Axios/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import axios from "axios"
+
+const App = () => {
+
+  const fetchData = async () => {
+
+    // const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+    // const data = await response.json();
+    // console.log(data);
+
+    // fetch("https://jsonplaceholder.typicode.com/posts")
+    // .then((response) => response.json())
+    // .then((data) => console.log(data))
+
+    const data = await axios.get("https://jsonplaceholder.typicode.com/posts")
+    console.log(data);
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center text-2xl mt-10">
+      <button onClick={fetchData} className="border-2 rounded-full px-7 py-3 cursor-pointer">Fetch Posts</button>
+    </div>
+  )
+}
+
+export default App
