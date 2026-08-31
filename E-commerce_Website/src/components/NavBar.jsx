4e19928dfc490e682dc0394e7599f450566eb395
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-08-31 10:10:03 +0530

    commit changes

---
+++ E-commerce_Website/src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../assets/store-logo.png'
+const NavBar = () => {
+  return (
+    <div className="flex items-center justify-between p-4 text-3xl gap-6 bg-blue-950 text-white font-serif">
+    <div>
+        <img src={logo} alt="logo"  className="w-18 h-16 object-contain"/>
+    </div>
+      <div className="flex items-center justify-center gap-10">
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ E-commerce_Website/src/main.jsx
@@ -0,0 +1,29 @@
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {createBrowserRouter} from 'react-router-dom'
+import Home from './components/Home.jsx'
+import Products from './components/Products.jsx'
+import {RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/products",
+        element: <Products/>
+      }
+    ]
+  }
+])
+createRoot(document.getElementById('root')).render(
+  <>
+    <RouterProvider router={router}/>
+  </>,
+)
